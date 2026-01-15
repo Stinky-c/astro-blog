463681c488c2a18a5d84f5a81419a413cf9595fc
Stinky-c <60587749+Stinky-c@users.noreply.github.com>
2026-01-15 13:23:28 -0800

    tinkering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), vue()],
+  integrations: [mdx(), icon(), vue()],
 });
